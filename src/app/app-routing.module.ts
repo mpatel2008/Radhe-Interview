e6f3bb2aa6516080ac245f49cabd46160d5d9151
mpatel <muneshpatel2008@gmail.com>
2022-12-06 00:38:11 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { QuestionsComponent } from './questions/questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'home',
+  component:DashboardComponent
+},
+{
+  path:'questions',
+  component:QuestionsComponent
+},
+
+{
+  path:'**',
+  component:NotFoundComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavComponent,
+    FooterComponent,
+    NotFoundComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
